8a37e09da553d164f4861e1e757c3b28c72dc4de
0xMarvell <rokemarvellous@gmail.com>
2023-03-07 13:51:53 +0100

    init

--- main.go
+++ main.go
@@ -4,13 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-
-	r.GET("/:name", func(ctx *gin.Context) {
-		name := ctx.Params.ByName("name")
-		ctx.JSON(200, gin.H{
-			"msg": "hello " + name,
-		})
-	})
-
 	r.Run()
 }
